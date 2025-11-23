56b6bbee77eeec50595683f3cf21451c669f01dc
Migu3lzin <miguelluisjp2409@gmail.com>
2025-11-23 08:31:45 -0300

    Update index.php
    Co-Authored-By: Eduardo Augusto <201088077+MestreDosMagos-6@users.noreply.github.com>

--- Projetoprincipal/intra/index.php
+++ Projetoprincipal/intra/index.php
@@ -7,7 +7,7 @@ if ($_POST) {
     $_SESSION['username'] = $_POST['username'];
 }
 
-// Redirecionar para o menu se estiver logado
+
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
     header('Location: ../core/menu.php');
     exit;
